35a7b3aba65d715bb911861a3b2fbec60280fe1a
zesciati <zakyabiyyu2003@gmail.com>
2026-05-22 15:40:31 +0700

    adding json file for repetitive stuff

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,31 +1,30 @@
-import ImageDaniel from "../assets/image-daniel.jpg";
+// import ImageDaniel from "../assets/image-daniel.jpg";
+import Testimonials from "../data/testimonials.json";
 
 function card() {
   return (
-    <div>
-      <div className="bg-purple-500 space-y-4 rounded-lg p-8  text-white">
-        <div className="flex gap-6">
-          <img src={ImageDaniel} alt="foto daniel" className="size-12 rounded-full border-2 border-purple-300"/>
+    <div className="space-y-5">
+      {Testimonials.map((testimonial) => (
+        <div className={`${testimonial.bgColor} space-y-4 rounded-lg p-8 ${testimonial.textColor}`}>
+          <div className="flex gap-6">
+            <img
+              src={testimonial.image}
+              alt="foto daniel"
+              className="size-12 rounded-full border-2 border-purple-300"
+            />
+            <div>
+              <p>{testimonial.name}</p>
+              <p>{testimonial.position}Verified Graduate</p>
+            </div>
+          </div>
+          <div className="text-2xl font-semibold">
+            {testimonial.title}
+          </div>
           <div>
-            <p>Daniel Clifford</p>
-            <p>Verified Graduate</p>
+            "{testimonial.desc}"
           </div>
         </div>
-        <div className="text-2xl font-semibold">
-          I received a job offer mid-course, and the subjects I learned were
-          current, if not more so, in the company I joined. I honestly feel I
-          got ever penny's worth.
-        </div>
-        <div>
-          "I was an EMT for many years before I joined the bootcamp. rve been
-          looking to make a transition and have heard some people who had an
-          amazing experience here. I signed up for the free intro course and
-          found it incrediblyfun! I enrolled shortly thereafter The next 12
-          weeks was the best - and most grueling - time of my life. Since
-          completing the course, I've successfully switched careers, working as
-          a Software Engineer at a VR startup."
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
